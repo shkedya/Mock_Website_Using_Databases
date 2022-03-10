e1cae7eed3a7679b81c02bacc93ea6820d82d645
PWiederspan <wiedersp@oregonstate.edu>
2022-03-10 10:37:28 -0800

    Adding Flask CRUD operations

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, flash
 from flask_mysqldb import MySQL
 from flask import request
+import datetime
 
 # Configuration
 app = Flask(__name__)
@@ -22,7 +23,7 @@ def root():
     return render_template("/index.j2")
 
 # Customers Page
-@app.route('/customer.html', methods=["POST", "GET"])
+@app.route('/customers.html', methods=["POST","GET"])
 def customers():
     """ 
     Read, and Create functionality for Customers page.
@@ -42,7 +43,7 @@ def customers():
             mysql.connection.commit()
 
             # redirect back to customer page
-            return redirect("/customer.html")
+            return redirect("/customers.html")
 
     # Get Customer data from Customer Table
     if request.method == "GET":
@@ -54,6 +55,18 @@ def customers():
 
         # Render the Customers page with the fetched data
         return render_template("customers.j2", customer_data = customer_table)
+
+#Delete customer based on ID
+@app.route('/delete-customer/<int:id>')
+def delete_customer(id):
+    cur = mysql.connection.cursor()
+    query = "DELETE FROM Customers WHERE Customer_id =%s"
+    cur.execute(query, (id,))
+    mysql.connection.commit()
+    flash("User deleted successfully")
+    return redirect("/customers.html")
+
+
     
 # Items table
 @app.route('/items.html', methods=["POST", "GET"])
@@ -64,14 +77,14 @@ def items():
     # Add an Item to the database 
     if request.method == "POST":                    
         if request.form.get("insert_item_submit"):
-            price = request.form["price"]
-            description = request.form["description"]
-            badges_req = request.form["badges_req"]
+            Price = request.form["Price"]
+            Description = request.form["Description"]
+            Badge_required = request.form["Badge_required"]
 
             #Insert new values into Items Table
-            query = "INSERT INTO Items (price, description, badges_req) VALUES (%s, %s, %s)"
+            query = "INSERT INTO Items (Price, Description, Badge_required) VALUES (%s, %s, %s)"
             cur = mysql.connection.cursor()
-            cur.execute(query, (price, description, badges_req))
+            cur.execute(query, (Price, Description, Badge_required))
             mysql.connection.commit()
 
             # redirect back to items page
@@ -85,40 +98,81 @@ def items():
         cur.execute(query_table)
         item_table = cur.fetchall()
 
-        # Render the Products page with the fetched data
+        # Render the items page with the fetched data
         return render_template("items.j2", item_data = item_table)
-    
+
 # Orders Page
 @app.route('/orders.html', methods=["POST", "GET"])
-def customers():
+def orders():
     """ 
     Read, and Create functionality for Orders page.
     """
     # Add a order to the database 
     if request.method == "POST":                    
-        if request.form.get("insert_item_submit"):
-            customer_id = request.form["customer_id"]
-            item_id = request.form["item_id"]
-            quantity = request.form["quantity"]
-            date = request.form["date"]
-            total = request.form["total"]
+        if request.form.get("insert_order_submit"):
+            Customer_id = request.form["Customer_id"]
+            Item_id = request.form["Item_id"]
+            Quantity = request.form["Quantity"]
+            Date = datetime.datetime.utcnow()
+            Total = request.form["Total"]
 
             #Insert new values into Customer Table
-            query = "INSERT INTO Orders (customer_id, item_id, quantity, date, total) VALUES (%s, %s, %s, %s, %s)"
+            query = "INSERT INTO Orders (Customer_id, Item_id, Quantity, Date, Total) VALUES (%s, %s, %s, %s, %s)"
             cur = mysql.connection.cursor()
-            cur.execute(query, (customer_id, item_id, quantity, date, total))
+            cur.execute(query, (Customer_id, Item_id, Quantity, Date, Total))
             mysql.connection.commit()
 
-            # redirect back to customer page
-            return redirect("/items.html")
+            # redirect back to orders page
+            return redirect("/orders.html")
 
     # Get Order data from Orders Table
     if request.method == "GET":
         # Query to populate table
-        query_table = "SELECT * FROM Items"
+        query_table = "SELECT * FROM Orders"
         cur = mysql.connection.cursor()
         cur.execute(query_table)
-        customer_table = cur.fetchall()
+        order_table = cur.fetchall()
 
         # Render the Order page with the fetched data
-        return render_template("items.j2", item_data = item_table)
+        return render_template("orders.j2", order_data = order_table)
+
+
+# Trades Page
+@app.route('/trades.html', methods=["POST", "GET"])
+def trades():
+    """ 
+    Read, and Create functionality for Trades page.
+    """
+    # Add a order to the database 
+    if request.method == "POST":                    
+        if request.form.get("insert_trade_submit"):
+            Sender = request.form["Sender"]
+            Receiver = request.form["Receiver"]
+
+            #Insert new values into Customer Table
+            query = "INSERT INTO Trades (Sender, Receiver) VALUES (%s, %s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (Sender, Receiver))
+            mysql.connection.commit()
+
+            # redirect back to trades page
+            return redirect("/trades.html")
+        
+
+    # Get Order data from Trades Table
+    if request.method == "GET":
+        # Query to populate table
+        query_table = "SELECT * FROM Trades"
+        cur = mysql.connection.cursor()
+        cur.execute(query_table)
+        trade_table = cur.fetchall()
+
+        # Render the Order page with the fetched data
+        return render_template("trades.j2", trade_data = trade_table)
+    
+
+app.secret_key = 'This is a secret'
+
+# Listener
+if __name__ == "__main__":
+    app.run(host="flip2.engr.oregonstate.edu", debug=True)
